925ff60c549ce18e57c07592828fc581bc9e8d51
J-Plum <98649953+J-Plum@users.noreply.github.com>
2025-09-02 23:59:01 +0900

    refactor : mock 데이터를 이용하기위해 리팩토링

--- app/weather/[city]/components/WeatherDisplay.js
+++ app/weather/[city]/components/WeatherDisplay.js
@@ -6,22 +6,22 @@ export default function WeatherDisplay({ city, weatherData }) {
   return (
     <div>
       <div className="flex flex-col items-center p-8 bg-white shadow-lg rounded-xl">
-        <h1 className="text-5xl font-bold mb-4 capitalize">{city}</h1>
-        <p className="text-8xl font-thin mb-2">{Math.round(weatherData.main.temp)}°C</p>
-        <p className="text-2xl mb-2">{weatherData.weather[0].description}</p>
+        <div className="text-5xl font-bold mb-4">{decodeURI(city)}</div>
+        <p className="text-8xl font-thin mb-2">{Math.round(forecast[0] && forecast[0].temp)}°C</p>
+        <p className="text-2xl mb-2">{forecast[0] && forecast[0].description}</p>
         <div className="flex justify-around w-full mt-4 text-gray-600">
           <div className="flex flex-col items-center">
             <p className="font-bold">최고 기온</p>
-            <p>{Math.round(weatherData.main.temp_max)}°C</p>
+            <p>{Math.round(forecast[0] && forecast[0].temp_max)}°C</p>
           </div>
           <div className="flex flex-col items-center">
             <p className="font-bold">최저 기온</p>
-            <p>{Math.round(weatherData.main.temp_min)}°C</p>
+            <p>{Math.round(forecast[0] && forecast[0].temp_min)}°C</p>
           </div>
         </div>
       </div>
       {/* TODO 인피니티 스크롤 추가할 부분 */}
-      <div className="mt-4 p-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-7 bg-[#f0f4f8] ">
+      <div className="mt-4 p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-7 bg-[#f0f4f8] ">
         {forecast.map((weatherData) => {
           return <WeatherCard key={weatherData.date} weatherData={weatherData} />;
         })}
--- app/weather/[city]/page.js
+++ app/weather/[city]/page.js
@@ -4,18 +4,15 @@ export default async function WeatherPage({ params }) {
 
   const weatherData = await getWeatherData(city);
 
-  return <WeatherDisplay weatherData={weatherData} city={city} />;
+  return <WeatherDisplay weatherData={city} city={city} />;
 }
 
+//loading.js 확인위해 주석처리 x
 async function getWeatherData(city) {
   const apiKey = process.env.WEATHER_API_KEY;
   const res = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=kr`
   );
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch weather data");
-  }
 
   return res.json();
 }
